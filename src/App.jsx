4bfbc3018908971f85435144fd82fca158fd1d15
gela pipia <gelapipia10@gmail.com>
2025-06-08 17:12:49 +0400

    ADD: title and subtitle and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import data from "./data";
 function App() {
   return (
     <div className="app">
-      {data.map((card) => (
-        <Card
-          key={card.id}
-          city_name={card.city_name}
-          date={card.date}
-          price={card.price}
-          month={card.month}
-          imageUrl={card.imageUrl}
-        />
-      ))}
+      <div>
+        <h1 className="title">ფრენები ყველა მიმართულებით</h1>
+        <p className="sub-title">დაბალბიუჯეტიანი ფრენები საქართველოდან ნებისმიერი მიმართულებით.</p>
+      </div>
+      <div className="cards-box">
+        {data.map((card) => (
+          <Card
+            key={card.id}
+            city_name={card.city_name}
+            date={card.date}
+            price={card.price}
+            month={card.month}
+            imageUrl={card.imageUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 }
