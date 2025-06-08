7e2988fe5dc855f2fe9d09c405b07bbc3a2d2c17
gela pipia <gelapipia10@gmail.com>
2025-06-08 17:02:26 +0400

    ADD: images and data

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    city_name: "პარიზი",
+    month: "სექ",
+    price: 520,
+    date: 28,
+    imageUrl: "/paris.png"
+  },
+  {
+    id: 2,
+    city_name: "ლონდონი",
+    month: "თებ",
+    price: 570,
+    date: 2,
+    imageUrl: "/london.png"
+  },
+  {
+    id: 3,
+    city_name: "თბილისი",
+    month: "აპრ",
+    price: 720,
+    date: 3,
+    imageUrl: "/tbilisi.png"
+  },
+];
+
+export default data;
